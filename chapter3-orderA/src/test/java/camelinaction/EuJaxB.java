724fe1cbedf5f66c4a1d098499d89861cf9729bd
Marius <Marius@Marius>
2018-02-01 22:43:20 +0200

    initial

---
+++ chapter3-orderA/src/test/java/camelinaction/EuJaxB.java
@@ -0,0 +1,57 @@
+package camelinaction;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class EuJaxB extends CamelTestSupport {
+
+    @Test
+    public void testJaxb() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:order");
+        mock.expectedMessageCount(1);
+        mock.message(0).body().isInstanceOf(PurchaseOrder.class);
+
+        PurchaseOrder order = new PurchaseOrder();
+        order.setName("Camel in Action");
+        order.setPrice(4995);
+        order.setAmount(1);
+
+        template.sendBody("direct:order", order);
+
+        assertMockEndpointsSatisfied();
+    }
+
+   
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+          //vezi ca nu e bine 
+            	
+                from("direct:order")
+                .marshal().jacksonxml()
+                .to("mock:order");
+           
+
+       
+      
+
+            }
+        };
+    }
+
+
+
+}
